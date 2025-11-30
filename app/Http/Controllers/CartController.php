4a26d9cd03a2059ca6f985e176bbe4bef0cdb6e0
EllisHannaAston <240267573@aston.ac.uk>
2025-11-30 19:44:43 +0000

    Creates a cart
    Makes a cart and shows it according to the user ID

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
+use App\Models\Product;
+use App\Models\CartItem;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    public function showCart(): View
+    public function showCart(Request $request): View
     {
+        $cart = Cart::where('UID', $request->user()->UID)
+            ->with('items.product')
+            ->first();
+              
         return view('cart');
+    }
+
+    public function makeCart(Request $request)
+    {
+        $cart = Cart::create([
+            'UID'=> $request->user()->UID,
+            'Total_Price' => 0
+        ]);
+
+        return $cart;
     }
 }

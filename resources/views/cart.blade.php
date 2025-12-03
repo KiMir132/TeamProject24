a6f5465c471024ba3c9185076c105aab20a0af90
EllisHannaAston <240267573@aston.ac.uk>
2025-12-03 22:12:22 +0000

    Basis for cart page. css folder setup
    Made css folder too for different css file per page

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,26 +1,44 @@
-<h1>Your Cart</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Quipment</title>
+    <link rel="stylesheet" href="{{ asset('css/cart.css') }}">
+</head>
 
-<div>
-    @if(!$cart || $cart->items->isEmpty())
-        <p>Your cart is empty.</p>
-    @else
-        @foreach($cart->items as $item)
-            <div>
-                <strong>{{ $item->product->Name }}</strong><br>
-                Quantity: {{ $item->Quantity }}<br>
-                Price: £{{ number_format($item->product->Price * $item->Quantity, 2)}}<br>
+<body>
+    <nav>
+        NAV BAR GOES HERE
+    </nav>
 
-                <form action="{{ route('cart.remove', $item->product->ProductID) }}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">Remove</button>
+    <section id = "cart-section">
+        <h1>Your Cart</h1>
+        <div>
+            @if(!$cart || $cart->items->isEmpty())
+                <p>Your cart is empty.</p>
+            @else
+                @foreach($cart->items as $item)
+                    <div>
+                        <strong>{{ $item->product->Name }}</strong><br>
+                        Quantity: {{ $item->Quantity }}<br>
+                        Price: £{{ number_format($item->product->Price * $item->Quantity, 2)}}<br>
+
+                        <form action="{{ route('cart.remove', $item->product->ProductID) }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">Remove</button>
+                        </form>
+                    </div>
+                @endforeach
+
+                <h3>Total: £{{ $cart->items->sum('Price') }}</h3>
+                <form action="{{ route('checkout.form') }}" method="get">
+                    <button type="submit">Checkout</button>
                 </form>
-            </div>
-        @endforeach
+            @endif
+        </div>
+    </section>
+</body>
 
-        <h3>Total: £{{ $cart->items->sum('Price') }}</h3>
-        <form action="{{ route('checkout.form') }}" method="get">
-            <button type="submit">Checkout</button>
-        </form>
-    @endif
-</div>
+</html>

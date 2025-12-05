67a96e5d2b15a069d0734fd0a83853dc11ca5042
Muz555r <muzakiryounis786@gmail.com>
2025-12-05 12:51:20 +0000

    added light mode/dark mode button to welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,15 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <header class="navbar">
+    <!-- your logo / links here -->
+
+    <button id="theme-toggle" class="theme-toggle">
+        ☀️ Light mode
+    </button>
+</header>
+
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />

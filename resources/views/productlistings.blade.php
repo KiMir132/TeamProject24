ada2db9447ba004ccf8b8d22576663e744f1d233
vishalprab06 <240199621@aston.ac.uk>
2026-03-05 15:31:30 +0000

    changed title

--- resources/views/productlistings.blade.php
+++ resources/views/productlistings.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Products - E-quipment</title>
-    <link rel="stylesheet" href="product listings.css">
+    <link rel="stylesheet" href="{{ asset('css/productlistings.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
 </head>
 <body>

4e1309303cbc0bc0e2cee9bcd2246b44be765074
vishalprab06 <240199621@aston.ac.uk>
2026-03-22 13:35:43 +0000

    changed names

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Terms & Conditions – E-Quipment')
 
 @section('styles')
-    <link rel="stylesheet" href="{{ asset('css/t&c.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/terms.css') }}">
 @endsection
 
 @section('content')

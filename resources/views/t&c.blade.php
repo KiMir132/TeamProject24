b631ac20ac4fd1715e832b70a522196a979fb6d3
vishalprab06 <240199621@aston.ac.uk>
2026-03-22 13:23:12 +0000

    created terms and conditions page

---
+++ resources/views/t&c.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Terms & Conditions – E-Quipment')
+
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('css/t&c.css') }}">
+@endsection
+
+@section('content')
+    
+@endsection
